fba19f15c2cd6bbdf391d3aba3f22068e55eb4c5
Justin Barry <justin.barry.e@gmail.com>
2016-07-22 15:53:24 -0400

    init commit. project structure and files

---
+++ src/main/java/justin/java/practice/Primes.java
@@ -0,0 +1,28 @@
+package justin.java.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Primes {
+	public static void main(String[] args) {
+		System.out.println(primes(10).toString());
+	}
+	
+	public static List<Integer> primes(int n) {
+		List<Integer> primeList = new ArrayList<Integer>();
+		for (int i = 1; i < n; i++) {
+			if (isPrime(i))
+				primeList.add(i);
+		}
+		return primeList;
+	}
+	
+	public static boolean isPrime(int n) {
+		if (n == 1 || n == 2 || n == 3)
+			return true;
+		for (int i = 2; i < n; i++)
+			if (n % i == 0)
+				return false;
+		return true;
+	}
+}
---
+++ src/test/java/justin/java/practice/PrimeTest.java
@@ -0,0 +1,28 @@
+package justin.java.practice;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hamcrest.collection.IsIterableContainingInOrder;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PrimeTest {
+	@Test
+	public void isPrimeTest() {
+		assertTrue(Primes.isPrime(7));
+	}
+
+	@Test
+	public void primeListTest() {
+		List<Integer> testPrimeList = new ArrayList<Integer>();
+		testPrimeList.add(1);
+		testPrimeList.add(2);
+		testPrimeList.add(3);
+		testPrimeList.add(5);
+		testPrimeList.add(7);
+		Assert.assertThat(Primes.primes(10), IsIterableContainingInOrder.contains(testPrimeList.toArray()));
+	}
+}
