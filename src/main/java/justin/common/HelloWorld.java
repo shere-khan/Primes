e7743f56dfb00c6bc86241c874cbeeeb9eceaa84
Justin Barry <justin.barry.e@gmail.com>
2016-07-23 06:52:46 -0400

    Added Jersey REST Hello World

--- src/main/java/justin/common/HelloWorld.java
+++ src/main/java/justin/common/HelloWorld.java
@@ -3,7 +3,6 @@ package justin.common;
 public class HelloWorld {
 
 	public HelloWorld() {
-		System.out.println("Hello World form Jetty!");
 	}
 	public void printHelloWorld() {
 		System.out.println("Hello World");
