2cb2ec0dd1f0152fb7e8f65777008be67ff55789
Justin Barry <justin.barry.e@gmail.com>
2016-07-23 00:43:18 -0400

    Added Web initializer. Started web application

--- src/main/java/justin/common/HelloWorld.java
+++ src/main/java/justin/common/HelloWorld.java
@@ -2,6 +2,9 @@ package justin.common;
 
 public class HelloWorld {
 
+	public HelloWorld() {
+		System.out.println("Hello World form Jetty!");
+	}
 	public void printHelloWorld() {
 		System.out.println("Hello World");
 	}
--- src/main/java/justin/webapplication/WebApplication.java
+++ src/main/java/justin/webapplication/WebApplication.java
@@ -1,16 +1,21 @@
 package justin.webapplication;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
 
 import justin.common.HelloWorld;
 
-public class WebApplication {
-	public static void main(String[] args) {
+public class WebApplication implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext arg0) throws ServletException {
 		@SuppressWarnings("resource")
 		ApplicationContext context = new ClassPathXmlApplicationContext("application-config.xml");
-		
+
 		HelloWorld helloWorldBean = (HelloWorld) context.getBean("helloWorldBean");
-		helloWorldBean.printHelloWorld();
 	}
 }
